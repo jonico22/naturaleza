14e4b05139f192b26559f33a3daecc60336d6fe2
Jose Luis <joseyananicolas@gmail.com>
2024-03-02 11:33:07 -0500

    feat: update pasos

--- src/components/home/feature/feature.tsx
+++ src/components/home/feature/feature.tsx
@@ -4,14 +4,13 @@ export const Feature = () => {
     <section
       className="m-auto grid w-full max-w-screen-2xl grid-flow-row flex-wrap items-center gap-x-0 gap-y-6 px-3 
       py-6 text-center md:gap-x-14 md:px-16 md:py-14 lg:grid-cols-2 lg:text-start "
->
+      >
       <div
         className="flex w-full max-w-2xl items-center justify-center justify-self-end rounded-2xl  md:py-12"
       >
         <img
           src="/feature2.svg"
           alt=""
-          
         />
       </div>
       <div className="flex flex-col gap-6 justify-self-start md:gap-12">
--- src/components/home/main/main.tsx
+++ src/components/home/main/main.tsx
@@ -4,24 +4,20 @@ export const Main = () => {
       className="flex size-full items-start justify-center bg-cover bg-center bg-no-repeat"
 >
       <div
-        className="flex max-w-screen-2xl flex-1 grow flex-col items-start justify-start gap-12 px-3 py-12 md:px-24 md:pt-24 xl:flex-row"
+        className="flex max-w-screen-2xl flex-1 grow flex-col-reverse items-start justify-start gap-12 px-3 md:px-12 xl:flex-row"
       >
-        <div className="lx:px-36 mb-0 flex flex-1  xl:mb-24">
-          <div className="flex max-w-lg flex-col gap-6 items-center justify-center">
-            <h3 className="text-2xl font-semibold text-slate-950 md:text-4xl mt-8">
+        <div className="lx:px-36 mb-0 flex flex-1 ">
+          <div className="flex  flex-col gap-6 items-center justify-center w-full" style={{height:'58vh'}}>
+            <h3 className="text-2xl font-semibold text-slate-950 lg:text-4xl" style={{textAlign:'center'}}>
             Generar conciencia social e involucrar a la sociedad en la defensa de nuestros recursos naturales
             </h3>
           </div>
           
         </div>
-        <div
-          className="relative flex flex-1 flex-col w-full items-center justify-center  "
-        >
+        <div className="relative flex flex-1 flex-col w-full items-center justify-center" style={{minHeight:'58vh'}}>
            <img
           src="/main.png"
           alt="main"
-          className="absolute"
-          style={{top:'-8em'}}
         />
         </div>
       </div>
